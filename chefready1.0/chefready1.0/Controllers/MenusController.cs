494ad57973be2435a2712d0eaeddb4583a3ba20f
ggrossi <greg@submittable.com>
2017-02-04 22:37:27 -0700

    Establishing inidvidual pages
    Menu Items Features

---
+++ chefready1.0/chefready1.0/Controllers/MenusController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace chefready1._0.Controllers
+{
+    public class MenusController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
